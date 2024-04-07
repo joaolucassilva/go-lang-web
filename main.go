a1891473655e5ce988a3adb9d58a41d318549d53
joaolucassilva <luccas332@gmail.com>
2024-04-07 12:27:34 -0300

    chore: add connection postgres

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"database/sql"
+	_ "github.com/lib/pq"
 	"html/template"
 	"net/http"
 )
+
+func conectaComBancoDeDados() *sql.DB {
+	conexao := "user=postgres dbname=alura_loja password=1234 host=localhost port=5432 sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
 
 type Produto struct {
 	Nome       string
@@ -15,6 +26,8 @@ type Produto struct {
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	db := conectaComBancoDeDados()
+	defer db.Close()
 	http.HandleFunc("/", index)
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
